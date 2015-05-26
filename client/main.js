1be723694fd2fe725176acfa68873bfb06edfe54
Titus Soporan <titus@tsoporan.com>
2015-05-26 00:01:47 -0400

    displaying data + some restructure

--- client/main.js
+++ client/main.js
@@ -1,7 +1,15 @@
 // readDaily client-side code.
 
+Meteor.subscribe('books');
+
 Template.body.helpers({
   year: function() {
     return new Date().getFullYear();
+  }
+});
+
+Template.booksList.helpers({
+  books : function() {
+    return Books.find();
   }
 });
--- server/readDaily.js
+++
@@ -1,6 +0,0 @@
-// readDaily server-side code
-
-Meteor.startup(function () {
-  // code to run on server at startup
-  console.log("Starting up =)");
-});
