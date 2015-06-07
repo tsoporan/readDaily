71fe9c89b4a3b889b06d6b6aae2292d15eece28f
Titus Soporan <titus@tsoporan.com>
2015-06-07 14:52:26 -0400

    book create #1

--- client/main.js
+++ client/main.js
@@ -11,3 +11,25 @@ Template.booksList.helpers({
     return Books.find();
   }
 });
+
+Template.booksCreate.events({
+  'submit form' : function(e) {
+    e.preventDefault();
+
+    var title = $(e.target).find('[name=title]').val();
+ 
+
+    var book = {
+      createdAt: new Date(),
+      title : title,
+      author : $(e.target).find('[name=author]').val(),
+      pages: $(e.target).find('[name=pages]').val(),
+      slug : s.slugify(title),
+    };
+
+    book._id = Books.insert(book);
+
+    Router.go('book', book);
+
+  },
+})
